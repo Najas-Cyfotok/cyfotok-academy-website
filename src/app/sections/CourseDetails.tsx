b4261e1304abadc94d92499cf28f410c3a52074a
Najas-Cyfotok <najasnazar@cyfotok.academy>
2024-12-13 17:58:42 +0530

    indiviual course page completed with 2 sec skelton loader

--- src/app/sections/CourseDetails.tsx
+++ src/app/sections/CourseDetails.tsx
@@ -1,11 +1,12 @@
-
 import Image from "next/image";
 import { coursesData } from "../assets/courses";
 import { CalendarDays, LetterText, School, Trophy } from "lucide-react";
 import ShineBorder from "@/components/ui/shine-border";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { whatsapp } from "../assets/assets";
+import { facilitiesData, whatsapp } from "../assets/assets";
+import CourseDescription from "./CourseDescription";
+import FacilitiesCard from "../components/facilitiesCard";
 
 type CoursesProps = {
   params: {
@@ -36,8 +37,12 @@ const CourseDetails = ({ params }: CoursesProps) => {
             />
           </div>
           <div className="w-1/2 flex flex-col justify-center gap-6 max-xl:gap-3 text-white h-full max-lg:w-full">
-            <h2 className="text-4xl font-bold max-xl:text-2xl">{courseData.title}</h2>
-            <p className="font-semibold text-base max-xl:text-sm">{courseData.metaDesc}</p>
+            <h2 className="text-4xl font-bold max-xl:text-2xl">
+              {courseData.title}
+            </h2>
+            <p className="font-semibold text-base max-xl:text-sm">
+              {courseData.metaDesc}
+            </p>
             <ul className="flex items-center justify-between">
               <li className="font-semibold flex gap-2 text-sm">
                 <CalendarDays className="w-5 h-5 " />
@@ -51,8 +56,10 @@ const CourseDetails = ({ params }: CoursesProps) => {
                 <li className="max-sm:hidden">
                   <span
                     className={`${
-                      courseData.state == "Popular" ? "bg-orange-700"
-                    : "bg-green-700"} px-2 py-1 rounded-lg max-xl:text-sm`}
+                      courseData.state == "Popular"
+                        ? "bg-orange-700"
+                        : "bg-green-700"
+                    } px-2 py-1 rounded-lg max-xl:text-sm`}
                   >
                     {courseData.state}
                   </span>
@@ -69,17 +76,36 @@ const CourseDetails = ({ params }: CoursesProps) => {
             </p>
 
             <div className="flex gap-2">
-              <Button className="px-5 py-2 text-xl font-semibold bg-black text-blacktext-white max-sm:text-base">Book Demo</Button>
-             <Link href={"https://wa.me/+919167121201"} target="_blank">
-             <Button className="px-5 py-2 text-xl font-semibold bg-green-700 text-white hover:bg-green-800 flex items-center max-sm:text-base">
-                <Image src={whatsapp} alt="whatsapp" width={20} height={20} className="w-8 h-8 max-sm:w-5 max-sm:h-5"/>
-                Chat With WhatsApp</Button>
-             </Link>
+              <Button className="px-5 py-2 text-xl font-semibold bg-black text-blacktext-white max-sm:text-base">
+                Book Demo
+              </Button>
+              <Link href={"https://wa.me/+919167121201"} target="_blank">
+                <Button className="px-5 py-2 text-xl font-semibold bg-green-700 text-white hover:bg-green-800 flex items-center max-sm:text-base">
+                  <Image
+                    src={whatsapp}
+                    alt="whatsapp"
+                    width={20}
+                    height={20}
+                    className="w-8 h-8 max-sm:w-5 max-sm:h-5"
+                  />
+                  Chat With WhatsApp
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
-        
       </ShineBorder>
+      <CourseDescription
+        desc={courseData.desc}
+        require={courseData.requirement}
+        faq={courseData.faq}
+        lessonItems={courseData.lessonItems}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-md:px-3">
+        {facilitiesData.map((item, index) => (
+          <FacilitiesCard key={index} {...item} />
+        ))}
+      </div>
     </section>
   );
 };
